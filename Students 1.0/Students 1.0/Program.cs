9e5089305401c5d294080c774ae004f18e0964bc
jageshj <jageshj@gmail.com>
2021-11-23 04:25:13 +0000

    This is Jagesh jha first commit

---
+++ Students 1.0/Students 1.0/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Students_1._0
+{
+	class Program
+	{
+		static void Main()
+		{
+			runApp();
+		}
+		public static void runApp()
+		{
+			//string fileName = @"Files_Q3.txt";
+		
+			StreamReader sr = new StreamReader(@"C:\\FSD\\Files\\Students.txt");
+			string i = sr.ReadToEnd().ToString();
+
+			string[] lines = i.Split('\n');
+
+            Console.WriteLine("Before Sorting");
+            Console.WriteLine("===============================");
+			foreach (string s in lines)
+			{
+				Console.WriteLine(s);
+			}
+			Console.WriteLine("===============================");
+			
+			Array.Sort(lines);
+			Console.WriteLine("After Sorting");
+			Console.WriteLine("===============================");
+			foreach (string s in lines)
+            {
+                Console.WriteLine(s);
+            }
+
+			//Console.WriteLine(i);
+		// string[] filedata = new string[i];
+
+// var lines = sr.ReadLine();
+//foreach (string s in lines.ToString()) 
+//          {
+// Console.WriteLine();
+//       }
+
+
+
+		}
+	}
+}
