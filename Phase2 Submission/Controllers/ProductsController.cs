77cfc9df3121870a0fd4b7f22a764046e041d240
Jagesh Jha <Jagesh_Jha@Dell.com>
2022-04-28 13:23:25 -0500

    MIgrated from https://github.com/jageshj/MumbaiPharma.git

---
+++ Phase2 Submission/Controllers/ProductsController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
+using Phase2_Submission.Model;
+using System.Data.Entity;
+
+namespace MumbaiPharma2.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        // private readonly IBaseRepository<UsersController> _UserRepository;
+        private readonly ILogger<ProductsController> _logger;
+        private readonly FSDAssignmentContext _db;
+        private readonly IJwtAuth _jwtAuth;
+
+        // private readonly BaseRepository<UserModel> UserRepository;
+
+        public ProductsController(ILogger<ProductsController> logger, FSDAssignmentContext db, IJwtAuth jwtAuth)
+        {
+            _logger = logger;
+            _db = db;
+            _jwtAuth = jwtAuth;
+
+        }
+
+
+
+
+        [HttpGet()]
+
+        //   [Authorize(Policy = "Isadmin")]
+        [AllowAnonymous]
+        public List<Product> Get()
+        {
+            return _db.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        //[Authorize(Policy = "Isadmin")]
+        [AllowAnonymous]
+        public Product Get(int id)
+        {
+            return _db.Products.Find(id);
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        //[Authorize(Policy = "Isadmin")]
+        public string Put(Product model)
+        {
+            _db.Products.Add(model);
+            _db.SaveChanges();
+
+            return "Success";
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost()]
+        public IActionResult Update([FromBody] Product item)
+        {
+            if (item.ProductId == 0)
+            {
+                return BadRequest();
+            }
+            var Product = _db.Products.FirstOrDefault(t => t.ProductId == item.ProductId);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            Product.Name = item.Name;
+            Product.CompanyName = item.CompanyName;
+            Product.Price = item.Price;
+            Product.Quantity = item.Quantity;
+            Product.ImageUrl = item.ImageUrl;
+            Product.Uses = item.Uses;
+            Product.ExpireDate = item.ExpireDate;
+
+            _db.Products.Update(Product);
+            _db.SaveChanges();
+
+            return Ok(item.ProductId);
+        }
+
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        [Authorize(Policy = "Isadmin")]
+        public IActionResult Delete(int id)
+        {
+            Product products = _db.Products.Find(id);
+            if (products == null)
+            {
+                return Ok("Product not found");
+            }
+            else
+            {
+                _db.Remove(products);
+                _db.SaveChanges();
+            }
+
+            return Ok($"Succesfully Deleted {id}");
+
+        }
+
+        //private User GetCurrentUser()
+        //{
+        //    var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+        //    if(identity != null)
+        //    {
+        //        var userClaims = identity.Claims;
+        //        return new User
+        //        {
+        //            Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+        //            IsAdmin = bool.Parse(userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value)
+        //        };
+
+        //    }
+        //    return null;
+        //}
+
+    }
+}
---
+++ Phase2 Submission/Controllers/UsersController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Phase2_Submission.Model;
+using System.Security.Claims;
+
+namespace Phase2_Submission.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        // private readonly IBaseRepository<UsersController> _UserRepository;
+        private readonly ILogger<UsersController> _logger;
+        private readonly FSDAssignmentContext _db;
+        private readonly IJwtAuth _jwtAuth;
+
+        // private readonly BaseRepository<UserModel> UserRepository;
+
+        public UsersController(ILogger<UsersController> logger, FSDAssignmentContext db, IJwtAuth jwtAuth)
+        {
+            _logger = logger;
+            _db = db;
+            _jwtAuth = jwtAuth;
+
+        }
+
+
+        [AllowAnonymous]
+        // POST api/<MembersController>
+        [HttpPost("authentication")]
+        public IActionResult Authentication([FromBody] Login userCredential)
+        {
+            var token = _jwtAuth.Authentication(userCredential.Email, userCredential.Password, _db);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public List<User> Get()
+        {
+            return _db.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Policy = "Isadmin")]
+        public User Get(int id)
+        {
+            return _db.Users.Find(id);
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        public string Put(User model)
+        {
+            _db.Users.Add(model);
+            _db.SaveChanges();
+            return "Success";
+
+        }
+
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            User Usertodelete = _db.Users.Find(id);
+            if (Usertodelete == null)
+            {
+
+            }
+            else
+            {
+                _db.Remove(Usertodelete);
+                _db.SaveChanges();
+            }
+
+            return Ok($"Succesfully Deleted {id}");
+
+        }
+
+        private User GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new User
+                {
+                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                    IsAdmin = bool.Parse(userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value)
+                };
+
+            }
+            return null;
+        }
+
+    }
+}
